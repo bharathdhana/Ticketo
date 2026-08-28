56f03c0edec8ff4ce50466dc7f0e5def5d6347dc
Bharath Kumar J N <bharathdhanavan2612@gmail.com>
2026-08-29 02:01:37 +0530

    Initial commit

---
+++ src/test/java/com/bharath/ticketo/TicketoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bharath.ticketo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
